f07060bea16a44080fb64e451ba0882d24d5f375
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2023-03-02 10:58:52 -0600

    scaffolded about page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from './components/Footer';
 
 
 import HomePage from './pages/HomePage';
+// import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <HomePage />
+      {/* <AboutPage /> */}
       <Footer />
     </div>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import About from '../components/About';
+
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <About />
+        </main>
+    )
+}
+
+export default AboutPage
