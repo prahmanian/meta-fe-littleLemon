62340631284f5309dc45b44fec858a25016894d3
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2023-03-03 15:28:11 -0600

    refactored nav a to NavLink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import logo from '../assets/LittleLemonLogo.svg';
 import navLinks from '../NavLinks';
+import {NavLink} from 'react-router-dom';
 
 
 const Nav = () => {
+    let activeStyle = {
+        textDecoration: "underline",
+    };
     return (
         <nav className='Nav h-flex'>
             <img src={logo} className="Nav-logo" alt="Little Lemon Logo" />
             <ul className='h-flex space-between'>
                 {navLinks.map(({title, path}) => 
-                    <li key={title}><a className="Nav-link" href={path} target="_blank" rel="noopener noreferrer"> {title} </a></li>
+                    <li key={title}><NavLink style={({isActive}) => isActive ? activeStyle : undefined} className="Nav-link" to={path} > {title} </NavLink></li>
                 )}
             </ul>
         </nav>
