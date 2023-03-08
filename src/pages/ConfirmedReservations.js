f3450a456c5f3cd86ce114944b8da2600650a59e
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2023-03-07 23:27:21 -0600

    styled confirmations

--- src/pages/ConfirmedReservations.js
+++ src/pages/ConfirmedReservations.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import {BsFillCalendarFill} from 'react-icons/bs'
 
 const ConfirmedReservations = ({reservations}) => {
 
 
     return (
-        <main>
-            <h1>You have {reservations.length} confirmed reservations!</h1>
-    
-            {reservations.map(rez => (
-                <article key={rez.date + rez.time}>
-                <h2>{rez.name}, party of {rez.guestCount}</h2>
-                <p>{rez.date.toLocaleDateString('en-US')}</p>
-                <p>{rez.time}</p>
+        <main className='Confirmations'>
+            <h1 className='title'>You have {reservations.length} confirmed reservations!</h1>
+            <section className="Confirmations-container">   
+                {reservations.map(rez => (
+                    <article key={rez.date + rez.time} className="Confirmations-rez">
+                        <span className='day'>{rez.date.toDateString().split(" ")[0]}</span>
+                        <div className="Rez-icon-container">
+                            
+                            <BsFillCalendarFill className='Rez-icon' />
+                        </div>
+                    
+                    <div className="Rez-details">
+                        <h2>{rez.specialOccasion !== 'None' ? `A special ${rez.specialOccasion.toLowerCase()} celebration!` : 'Great vibes and a nice Mediterranean meal'}</h2>
+                        <p>{rez.name}, party of {rez.guestCount}</p>
+                        <p>{rez.date.toLocaleDateString('en-US')} at {rez.time}</p>
+                        
+                    </div>
+                    
 
-                </article>
-            ))}
-
+                    </article>
+                ))}
+            </section>
             
         </main>
     )
