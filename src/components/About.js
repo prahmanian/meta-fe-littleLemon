0d24b75bbfe19cda0f04323c03142d873b7c4217
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2023-03-08 02:04:12 -0600

    styled About

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,19 @@ import MarioAndAdrianB from '../assets/MarioAndAdrianB.jpg'
 
 const About = () => {
     return (
-        <section className='About container h-flex space-between'>
-            <article className='About-article v-flex left'>
-                <h3 className='About-title title'>Little Lemon</h3>
-                <h4 className='About-subtitle subtitle'>Chicago</h4>
-                <p className='About-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mi massa, luctus quis ante pharetra, mollis bibendum turpis. Nulla volutpat condimentum commodo. Proin a risus bibendum, hendrerit purus sit amet, fermentum sapien. Quisque eu nisi iaculis turpis posuere commodo. Nullam eget purus mauris. Praesent ullamcorper finibus luctus. Maecenas efficitur ipsum mi, a vehicula urna lobortis ut. Quisque sollicitudin diam vitae aliquam pretium.</p>
-            </article>
-            <div>
-                <img src={MarioAndAdrianA} className="About-img" alt="Mario and Adrian at Work" width="250px"/>
-                <img src={MarioAndAdrianB} className="About-img" alt="Mario and Adrian at Work" width="250px"/>
+        <section className=' container '>
+            <div className='About contents h-flex space-between wrap'>
+                <article className='About-article v-flex left'>
+                    <h3 className='About-title title'>Little Lemon</h3>
+                    <h4 className='About-subtitle subtitle'>Chicago</h4>
+                    <p className='About-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mi massa, luctus quis ante pharetra, mollis bibendum turpis. Nulla volutpat condimentum commodo. Proin a risus bibendum, hendrerit purus sit amet, fermentum sapien. Quisque eu nisi iaculis turpis posuere commodo. Nullam eget purus mauris. Praesent ullamcorper finibus luctus. Maecenas efficitur ipsum mi, a vehicula urna lobortis ut. Quisque sollicitudin diam vitae aliquam pretium.</p>
+                </article>
+                <div className='About-images'>
+                    <img src={MarioAndAdrianA} className="About-img" alt="Mario and Adrian at Work" width="250px"/>
+                    <img src={MarioAndAdrianB} className="About-img" alt="Mario and Adrian at Work" width="250px"/>
+                </div>
             </div>
+            
         </section>
 
     )
